996b35f47af394cca8a405770e6e1c03339bca01
IamI <xinguangyao@gmail.com>
2017-12-27 16:32:03 +0800

    Fix

---
+++ Main.js
@@ -0,0 +1,5 @@
+module.exports.Deck = require("./Deck.js");
+module.exports.Environment = require("./Environment.js");
+module.exports.Card = require('./Card.js');
+module.exports.Replay = require('./Replay.js');
+module.exports.Set = require('./Set.js');
